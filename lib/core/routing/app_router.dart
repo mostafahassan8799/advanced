e9ce51cc30872a1a771de1d9df597b56497046ae
Mostafa Hassan <mostafahassan8799@gmail.com>
2023-12-20 22:28:13 +0200

    add routing and on boarding screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,30 @@
+import 'package:advanced/core/routing/routes.dart';
+import 'package:advanced/features/login/ui/screens/login_screen.dart';
+import 'package:advanced/features/onboarding/onboarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    //this arguments to be passed in any screen like this ( arguments as ClassName )
+    final arguments = settings.arguments;
+
+    switch (settings.name) {
+      case Routes.onBoardingScreen:
+        return MaterialPageRoute(
+          builder: (_) => const OnBoardingScreen(),
+        );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => const LoginScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (_) => Scaffold(
+            body: Center(
+              child: Text('No route defined for ${settings.name}'),
+            ),
+          ),
+        );
+    }
+  }
+}
